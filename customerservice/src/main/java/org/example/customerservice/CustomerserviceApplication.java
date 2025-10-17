2fdc84e181135ee19a7f61d902125fd3fcb7b4e8
salmaabakil <153354773+salmaabakil@users.noreply.github.com>
2025-10-17 22:34:59 +0100

    spring boot config

--- customerservice/src/main/java/org/example/customerservice/CustomerserviceApplication.java
+++ customerservice/src/main/java/org/example/customerservice/CustomerserviceApplication.java
@@ -1,13 +1,17 @@
 package org.example.customerservice;
 
+import org.example.customerservice.config.CustomerConfigParams;
 import org.example.customerservice.entities.Customer;
 import org.example.customerservice.repository.CustomerRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties(CustomerConfigParams.class)
 public class CustomerserviceApplication {
 
     public static void main(String[] args) {
