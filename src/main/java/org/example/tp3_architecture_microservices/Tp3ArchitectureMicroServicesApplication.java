86465230c2ca1e089ec812871e5b2c24128c873a
salmaabakil <153354773+salmaabakil@users.noreply.github.com>
2025-10-17 15:21:38 +0100

    Initial commit

---
+++ src/main/java/org/example/tp3_architecture_microservices/Tp3ArchitectureMicroServicesApplication.java
@@ -0,0 +1,13 @@
+package org.example.tp3_architecture_microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp3ArchitectureMicroServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tp3ArchitectureMicroServicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/tp3_architecture_microservices/Tp3ArchitectureMicroServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tp3_architecture_microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp3ArchitectureMicroServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
